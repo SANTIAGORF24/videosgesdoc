e48f2f8379d55999c6940c6cd07f20070ca59025
SANTIAGO RAMÍREZ FORERO <santiforero57@gmail.com>
2024-02-13 16:43:35 -0500

    charles

--- components/AcmeLogo.jsx
+++ components/AcmeLogo.jsx
@@ -1,4 +1,5 @@
+import Image from "next/image";
 import React from "react";
 export const AcmeLogo = () => (
-  <img src="/assets/img/logo.png" className="w-10" />
+  <Image src="/assets/img/logo.png" className="w-10" width={100} height={100} />
 );
--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -6,10 +6,10 @@ export function Banner() {
       <div className="flex flex-col items-center text-center">
         <div className="flex flex-col items-center py-36">
           <h1 className="text-9xl font-bold ">APRENDAMOS</h1>
-          <h1 className="text-9xl font-bold ">GESDCO</h1>
+          <h1 className="text-9xl font-bold ">GESDOC</h1>
           <h5 className="w-3/6 text-xl py-10 text-slate-500">
-            ¡Bienvenido a nuestra página dedicada a la campaña "Aprendamos
-            Gesdoc"! Aquí encontrarás tutoriales detallados para dominar el
+            Bienvenido a nuestra página dedicada a la campaña ¡Aprendamos
+            Gesdoc! Aquí encontrarás tutoriales detallados para dominar el
             correcto funcionamiento de GESDOC.
           </h5>
         </div>
