9482b38406deadef23371e65d56e0e12968cb5c8
SANTIAGO RAMÍREZ FORERO <santiforero57@gmail.com>
2024-02-27 16:49:16 -0500

    resposive

--- components/Capacitaciones/Basicos.js
+++ components/Capacitaciones/Basicos.js
@@ -3,28 +3,29 @@ import React from "react";
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 import ReactPlayer from "react-player";
 import "../../app/globals.css";
+
 export function Basic() {
   const customBorderStyle = {
     borderImage: "linear-gradient(to right, #fcd34d, #ff6384, #8a2be2)",
     borderImageSlice: 1,
   };
+
   return (
     <>
-      <div id="basicos">
+      <div id="basicos" className="py-10">
         <div className="flex flex-col items-center">
-          <h2 className="sm:text-7xl font-bold py-20 text-5xl">Radicación</h2>
+          <h2 className="sm:text-7xl font-bold text-5xl">Radicación</h2>
         </div>
       </div>
       <div className="flex justify-center">
-        <div className="sm:flex sm:justify-between sm:w-4/5 max-sm:w-full max-sm:p-6 max-sm:gap-6 max-sm:flex max-sm:flex-col">
-          <div>
-            {" "}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 max-w-7xl mx-auto">
+          <div className="w-full">
             <Card
               style={customBorderStyle}
               className="py-4 bg-transparent border-solid border-3"
             >
               <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                <p className="text-tiny uppercase font-bold text-white ">
+                <p className="text-tiny uppercase font-bold text-white">
                   Capitulo 2
                 </p>
                 <small className="text-white">memorando</small>
@@ -36,20 +37,19 @@ export function Basic() {
                 <ReactPlayer
                   url={"https://youtu.be/lBsLRmMn2uo"}
                   controls
-                  width={400}
-                  height={202}
+                  width="100%"
+                  height="auto"
                 />
               </CardBody>
             </Card>
           </div>
-          <div>
-            {" "}
+          <div className="w-full">
             <Card
               style={customBorderStyle}
               className="py-4 bg-transparent border-solid border-3 "
             >
               <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                <p className="text-tiny uppercase font-bold text-white ">
+                <p className="text-tiny uppercase font-bold text-white">
                   Capitulo 3
                 </p>
                 <small className="text-white">memorando</small>
@@ -61,20 +61,19 @@ export function Basic() {
                 <ReactPlayer
                   url={"https://youtu.be/ZuW73B7lEj8"}
                   controls
-                  width={400}
-                  height={202}
+                  width="100%"
+                  height="auto"
                 />
               </CardBody>
             </Card>
           </div>
-          <div>
-            {" "}
+          <div className="w-full">
             <Card
               style={customBorderStyle}
               className="py-4 bg-transparent border-solid border-3"
             >
               <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                <p className="text-tiny uppercase font-bold text-white ">
+                <p className="text-tiny uppercase font-bold text-white">
                   Nueva version
                 </p>
                 <small className="text-white">GESDOC</small>
@@ -86,8 +85,8 @@ export function Basic() {
                 <ReactPlayer
                   url={"https://youtu.be/xc_IkvEPGUg"}
                   controls
-                  width={400}
-                  height={202}
+                  width="100%"
+                  height="auto"
                 />
               </CardBody>
             </Card>
--- components/Frecuentes/Frecuentes.js
+++ components/Frecuentes/Frecuentes.js
@@ -3,24 +3,25 @@ import React from "react";
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 import ReactPlayer from "react-player";
 import "../../app/globals.css";
+
 export function Frecuentes() {
   const customBorderStyle = {
     borderImage: "linear-gradient(to right, #fcd34d, #ff6384, #8a2be2)",
     borderImageSlice: 1,
   };
+
   return (
     <>
-      <div id="basicos">
+      <div id="basicos" className="py-10">
         <div className="flex flex-col items-center">
-          <h2 className="sm:text-7xl font-bold py-20 text-5xl">
+          <h2 className="sm:text-7xl font-bold text-5xl">
             PREGUNTAS FRECUENTES
           </h2>
         </div>
       </div>
       <div className="flex justify-center">
-        <div className="sm:flex sm:justify-between sm:w-4/5 max-sm:w-full max-sm:p-6 max-sm:gap-6 max-sm:flex max-sm:flex-col">
-          <div>
-            {" "}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6 max-w-7xl mx-auto">
+          <div className="w-full">
             <Card
               style={customBorderStyle}
               className="py-4 bg-transparent border-solid border-3"
@@ -38,14 +39,13 @@ export function Frecuentes() {
                 <ReactPlayer
                   url={"https://youtu.be/-ckjcoPzwAU"}
                   controls
-                  width={400}
-                  height={202}
+                  width="100%"
+                  height="auto"
                 />
               </CardBody>
             </Card>
           </div>
-          <div>
-            {" "}
+          <div className="w-full">
             <Card
               style={customBorderStyle}
               className="py-4 bg-transparent border-solid border-3 "
@@ -63,8 +63,8 @@ export function Frecuentes() {
                 <ReactPlayer
                   url={"https://youtu.be/eHP-uMhT_o4"}
                   controls
-                  width={400}
-                  height={202}
+                  width="100%"
+                  height="auto"
                 />
               </CardBody>
             </Card>
